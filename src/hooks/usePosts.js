f08b9254e26c22935d8bcedf0cd740b335ed211e
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-07-15 16:10:20 +0300

    created my own hook

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,1 +1,24 @@
-export const 
+import { useMemo } from "react";
+
+export const useSortedPosts = (posts, sort) => {
+    const sortedPosts = useMemo(() => {
+		if (sort === "sorting by") {
+			return posts;
+		}
+		return [...posts].sort((a, b) =>
+			a[sort].localeCompare(b[sort])
+		);
+	}, [sort, posts]);
+    return sortedPosts
+}
+
+export const usePosts = (posts, sort, query) => {
+
+    const sortedPosts = useSortedPosts(posts, sort)
+	const sortedAndSearchedPosts = useMemo(() => {
+		return sortedPosts.filter((post) =>
+			post.title.toLowerCase().includes(query.toLowerCase())
+		);
+	}, [query, sortedPosts]);
+    return sortedAndSearchedPosts;
+}
